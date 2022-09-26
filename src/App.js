0987ec5c64adc2e51d4c78e71b2b59664cb31387
Kibuye24 <93917125+Kibuye24@users.noreply.github.com>
2022-09-26 22:48:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Bill, TotalCount, Header } from './components/components';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg h-screen'>
+      <Header />
+      <div className='grid grid-cols-2 border-2 border-transparent rounded-lg w-[900px] ml-[200px] bg-white'>
+        <Bill />
+        <TotalCount />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
